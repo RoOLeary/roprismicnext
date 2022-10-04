b0b554d4b206aac18f13adc278c239164d1b4ce7
Ronan <roleary81@gmail.com>
2022-10-04 12:17:11 +0200

    add preview to documenbt

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
+        <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=roprismicnxt"></script>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
