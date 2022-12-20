e0a2ea662287875d7e8f13b8284045a71d278a0d
Ronan <roleary81@gmail.com>
2022-12-20 19:20:18 +0100

    test model deployment

--- .slicemachine/assets/slices/Repeater/index.stories.js
+++ .slicemachine/assets/slices/Repeater/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Vessels","spans":[]}],"description":[{"type":"paragraph","text":"Sunt irure consequat consequat mollit ea est amet eiusmod ad sunt fugiat aliquip ipsum pariatur consequat. Tempor reprehenderit nulla ullamco aliqua proident mollit.","spans":[]}]},"slice_type":"repeater","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"repeater":[{"type":"paragraph","text":"Voluptate minim cupidatat ullamco tempor sint officia quis ex.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Unhappy","spans":[]}],"description":[{"type":"paragraph","text":"Est commodo sunt in nulla laborum id voluptate id nulla mollit pariatur irure culpa veniam.","spans":[]}]},"slice_type":"repeater","id":"_Default"}} />
 _Default.storyName = ''
